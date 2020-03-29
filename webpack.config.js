11a0a65ddfda7837b3373d7daec16955f9dbebc0
Nat Welch <nat@natwelch.com>
2020-03-29 17:15:45 +0000

    infra

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require("path");
+module.exports = {
+  mode: "production",
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "index.js",
+    libraryTarget: "commonjs2",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
